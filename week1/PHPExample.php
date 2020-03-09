4a8d2f99f7e8ae7366452ff3cf9489e9c6422a5e
duchunter <duc71098@gmail.com>
2020-03-09 18:21:34 +0700

    week 1

---
+++ week1/PHPExample.php
@@ -0,0 +1,12 @@
+<?php
+    // Basic syntax
+    print("Hello world<br/>");
+
+    // Create error
+    /*
+     *  print("Hello world)
+     */
+
+    // Generate HTML using \
+    print "<font color=\"blue\">abc</font>"
+?>
